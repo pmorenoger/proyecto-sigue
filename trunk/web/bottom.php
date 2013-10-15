5cd0a524dd9dd9c39467f43bc3388c7fde1f72cc
loko64z@gmail.com <loko64z@gmail.com@94efbc2a-9681-09ae-62ee-ccbec4989650>
2013-10-15 21:54:51 +0000

    Pagina principal y login.

--- trunk/web/bottom.php
+++ trunk/web/bottom.php
@@ -4,7 +4,12 @@
 ?>
 
 <div id="footer">
-  
+      <ul class="about">
+          <li>Acerca de la aplicación</li>
+              <ul>
+                <li>Pincha <a href="test.php" title="comenzar"> aquí</a> para ver una demo.</li>            
+              </ul>
+          </ul>
     
 </div>
 </body>
---
+++ trunk/web/default_test.php
@@ -0,0 +1,24 @@
+<?php
+require 'top.php';
+?>
+<div id="content">
+    <p> Este es el servidor del (mejor) proyecto de SI de la Facultad de Informática de la Universidad Complutense de Madrid.</p>
+    <p> Este servidor servirá de modelo de pruebas durante la fase de desarrollo de la aplicación SIGUE.</p>
+
+    <p>¿Qué es SIGUE?
+        <ul>
+            <li>Es un sistema de "Seguimiento de participación y asistencia a clase."</li>
+            <li>Es un gestor de Tokens para el profesor.</li>
+            <li>Es la manera más cómoda para un alumno de hacer efectivos sus Tokens.</li>
+            <li>Es la herramienta definitiva de gestión de las notas de asistencia.</li>
+        </ul>
+    </p>
+    <div>
+      Pincha <a href="test.php" title="comenzar"> aquí</a> para comenzar.
+    </div>
+</div>
+
+
+<?php
+include 'bottom.php';
+?>
