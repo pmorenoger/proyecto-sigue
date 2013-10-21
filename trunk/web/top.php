2c7ef562de1bd5e8caa4be5bedb6960f638a6f1d
loko64z@gmail.com <loko64z@gmail.com@94efbc2a-9681-09ae-62ee-ccbec4989650>
2013-10-21 20:15:38 +0000

    Formulario login principal. Copiados estilos de alumno. Fallo javascript en index.php que no detecto :S

--- trunk/web/top.php
+++ trunk/web/top.php
@@ -16,17 +16,17 @@ $options = new ClsOptions();
 
 <head>
     <meta HTTP-EQUIV="Content-Type" content="text/html;charset=utf-8" />
-    <link rel="stylesheet" href="/css/estilos.css">     
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">  
-    <!-- 
-        Este creo que no hace falta, pues la version mas reciente lo engloba
-    <script src="/scripts/jquery-1.8.2.min.js" type="text/javascript"></script>   -->
-    <script src="/scripts/jquery.validationEngine-es.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="/css/estilos.css"> 
+    <link rel="stylesheet" href="/css/alumnoCSS.css">
+    <link rel="stylesheet" type="text/css" href="/css/validationEngine.jquery.css">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">     
+    <!-- <script src="/scripts/jquery-1.8.2.min.js" type="text/javascript"></script> -->
+    <script src="/scripts/jquery.validationEngine.js" type="text/javascript"></script>
+    <script src="/scripts/jquery.validationEngine-es.js" type="text/javascript"></script>        
     <script src="/scripts/jquery-1.9.1.js" type="text/javascript"></script>
-    <script src="/scripts/jquery-ui-1.10.3.custom.js" type="text/javascript"></script>
-   
-    <script src="/scripts/jquery-ui-1.10.3.custom.min.js" type="text/javascript">
-    </script>
+    <script src="/scripts/jquery-ui-1.10.3.custom.js" type="text/javascript"></script>   
+    <script src="/scripts/jquery-ui-1.10.3.custom.min.js" type="text/javascript"></script>
+    <title>SIGUE</title>
 </head>
 <body>
     <div id="top">
