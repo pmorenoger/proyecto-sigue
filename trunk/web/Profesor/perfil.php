14090488d1efcf49a75984db5f3bc51d07f2db0e
loko64z@gmail.com <loko64z@gmail.com@94efbc2a-9681-09ae-62ee-ccbec4989650>
2013-10-21 19:07:23 +0000

    Parte del profesor, primer esbozo

---
+++ trunk/web/Profesor/perfil.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once("../top.php");
+?>
+<div id="content">
+    <div class="left_menu">
+        <span>Asignatura-Grupo</span>
+        <div id="accordion">
+            <h3>Algebra</h3>
+                 <div>
+                    <ul>           
+                        <li> <a href=""> Algebra 1ºA </a> </li>
+                        <li> <a href=""> Algebra 1ºB </a> </li>
+                    </ul>
+                 </div>
+            <h3>Fundamentos de Programación</h3>
+                <div>
+                    <ul>           
+                        <li> <a href=""> Fundamentos de Programación 1ºA</a> </li>
+                        <li> <a href=""> Fundamentos de Programación 1ºB </a> </li>
+                    </ul>
+                 </div>
+        </div>       
+    </div>
+    <div class="content_center">
+        AQUI IRA LA INFO Y LOS DATOS DE LOS DISTINTOS DEPARTAMENTOS DEL MENU DE LA DERECHA
+
+
+    </div>
+</div>
+
+<script>
+$("#accordion").accordion();
+</script>
+ 
+
+<?php
+
+require_once("../bottom.php");
+?>
