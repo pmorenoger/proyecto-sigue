498463ec0d821857083811a9548df6b057e4d397
hecmar091@gmail.com <hecmar091@gmail.com@94efbc2a-9681-09ae-62ee-ccbec4989650>
2013-12-11 23:17:02 +0000

    Se añadió la opción de mostrar las actividades.

--- trunk/web/src/SI/SigueBundle/Resources/views/Alumno/perfil.html.php
+++ trunk/web/src/SI/SigueBundle/Resources/views/Alumno/perfil.html.php
@@ -24,8 +24,9 @@
                             <h3>Otras Opciones</h3>
                                 <div>
                                     <ul>           
-                                        <li> <a href=""> Perfil </a> </li>
+                                        <li> <a href="#" onclick="mostrarActividades();"> Mostrar Actividades </a> </li>
                                         <li> <a href=""> Otros </a> </li>
+                                    </ul>
                                  </div>
                           </div>
                         </div>
@@ -34,6 +35,26 @@
                         <p><strong>El número total de tokens que tienes es: <?php echo $total ?></strong></p>
                         <div id="codQR">
 
+                        </div>
+                        <div id="actividades" class="hiddenStructure">
+                            <?php if($actividades !== NULL and count($actividades) >0): ?>
+                                <table border="1" style='text-align: center;'>
+                                    <th>Asignatura</th>
+                                    <th>Actividad</th>
+                                    <th>Descripción</th>
+                                    <th>Peso(%)</th>
+                                    <th>Puntuación</th>
+                                    <?php foreach ($actividades as $act) :?>
+                                        <tr>
+                                            <td><?php echo $act->getIdAsignatura()->getNombre(); ?></td>
+                                            <td><?php echo $act->getNombre(); ?></td>
+                                            <td><?php echo $act->getDescripcion(); ?></td>
+                                            <td><?php echo $act->getPeso()*100; ?></td>  
+                                            <td><?php echo $act->getPeso()*$act->getNota(); ?></td>    
+                                        </tr>
+                                    <?php endforeach; ?>
+                                </table>
+                            <?php endif; ?>
                         </div>
                     </td>
             </tr>
@@ -61,24 +82,36 @@
     });
     
     function qr(){
-        cod = '<?php echo $alumno->getCodigo_id(); ?>';
-        $.ajax({
-            type:"GET",
-            url: "../../../vendor/generadorQR.php",
-            async: true,
-            data: {
-               codigo: cod 
-            },
-            dataType:"json",
-            success: function(data) {
-                if (data.status){
-                    $("#codQR").append("<img src='../.." + data.dir + "'>");
-                    $("#bActivar").attr("disabled", "disabled");
-                }else{
-                    $("#bActivar").attr("disabled", "disabled");
-                }
-             }
-        });
+        if ($('#codQR').has('img').length){
+            $("#actividades").addClass('hiddenStructure');
+            $("#codQR").removeClass('hiddenStructure');
+        }else{
+            cod = '<?php echo $alumno->getCodigo_id(); ?>';
+            $.ajax({
+                type:"GET",
+                url: "../../../vendor/generadorQR.php",
+                async: true,
+                data: {
+                   codigo: cod 
+                },
+                dataType:"json",
+                success: function(data) {
+                    if (data.status){
+                        $("#actividades").addClass('hiddenStructure');
+                        $("#codQR").removeClass('hiddenStructure')
+                        $("#codQR").append("<img src='../.." + data.dir + "'>");
+                        //$("#bActivar").attr("disabled", "disabled");
+                    }else{
+                        $("#bActivar").attr("disabled", "disabled");
+                    }
+                 }
+            });
+        }
+    }
+    
+    function mostrarActividades(){
+        $('#codQR').addClass('hiddenStructure');
+        $('#actividades').removeClass('hiddenStructure');
     }
 </script>
 <?php $view['slots']->stop(); ?>
