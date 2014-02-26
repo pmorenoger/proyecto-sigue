615e8f6d779937290fe400aeb47740239908dcac
loko64z <loko64z@gmail.com>
2014-02-26 11:07:40 +0000

    Calificador exporta los excels perfectamente.

--- web/src/SI/SigueBundle/Controller/ProfesorController.php
+++ web/src/SI/SigueBundle/Controller/ProfesorController.php
@@ -53,7 +53,7 @@ class ProfesorController extends Controller
            }              
         }
         public function subir_alumnoAction(){
-              $kernel = $this->get('kernel');
+            $kernel = $this->get('kernel');
             $dir_abs = self::getDireccionAbsoluta();
             require_once $dir_abs . '/vendor/Excel/lib/src/Classes/PHPExcel.php';
             
@@ -67,10 +67,7 @@ class ProfesorController extends Controller
             
              $exito = "none";  
              if( $kernel->getEnvironment() === "dev" ){
-                 /*Si quereis que funcione en vuestro desarrollo localhost cambiad esta ruta.*/
-                 
-                //HECTOR:
-                //$uploaddir = 'C:\Users\j\Documents\proyecto-sigue\web\web\archivos';
+                 /*Si quereis que funcione en vuestro desarrollo localhost cambiad esta ruta.*/                                               
                  //DIEGO:
                  $uploaddir = self::getDireccionAbsoluta()."/web/archivos/";
                 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
@@ -330,53 +327,11 @@ class ProfesorController extends Controller
         }
         
         public function calificarAction($id_asignatura){            
-            $em = $this->getDoctrine()->getManager();
-            $repo = $em->getRepository('SISigueBundle:Asignaturas');
-            $asignatura = $repo->find($id_asignatura);  
-            //Tengo que sacar la lista de alumnos de la asignatura
-            //Luego, por cada alumno, saco la lista de actividades que tiene asociado
-            // y se lo añado a un array listo para mostrar.
             
-            $asig_alumnos = $em->getRepository('SISigueBundle:AsignaturaAlumno')->findByIdAsignatura($asignatura);
-            //Para poder mostrarlo correctamente necesito la info de las actividades, sin importar 
-            $actividades = $em->createQuery('
-                        SELECT distinct(p.nombre), p.descripcion, p.peso
-                        FROM SISigueBundle:ActividadAsignatura p                   
-                        
-                    ');
-            $actividades_info = $actividades->getResult();
-           // var_dump($actividades_info);
-           // die();
-            $resultado = array();
-            $parcial = array();
-            foreach($asig_alumnos as $alumnos){
-                $alumno = $alumnos->getIdAlumno();
-                //$repo_actividades = $em->getRepository('SISigueBundle:ActividadAsignatura');
-                $query = $em->CreateQuery(
-                        'SELECT p
-                         FROM SISigueBundle:ActividadAsignatura p
-                            WHERE p.idAsignatura = :idAsignatura  
-                            AND p.idAlumno = :idAlumno
-                            '
-                        )->SetParameters(array('idAsignatura'=>$id_asignatura, 'idAlumno'=>$alumno));
-                $actividades = $query->getResult();
-                $query2 = $em->CreateQuery(
-                            'SELECT cod
-                            FROM SISigueBundle:AsignaturaAlumno cod
-                            where cod.idAsignatura = :idAsignatura
-                            AND cod.idAlumno = :idAlumno
-                            ')->SetParameters(array('idAsignatura'=>$id_asignatura, 'idAlumno'=>$alumno));
-                $codigos = $query2->getResult();
-               
-                $parcial = array("alumno" => $alumno, "actividades" => $actividades, "codigos"=>$codigos);
-                array_push($resultado, $parcial);
-            }
-            //var_dump($resultado);
-            //die();
+            $array = self::listado_alumnos_actividad_asignatura($id_asignatura);
             
             
-            
-            return $this->render('SISigueBundle:Profesor:calificar.html.php', array("asignatura"=>$asignatura,"resultados"=>$resultado, "actividades"=>$actividades_info));
+            return $this->render('SISigueBundle:Profesor:calificar.html.php', $array);
         }
         
          public function calificar_actividadAction($id_asignatura, $id_alumno){            
@@ -458,7 +413,240 @@ class ProfesorController extends Controller
                $em->flush();
               return $this->calificarAction($id_asignatura);
            }
-         
+        
+        public function guardar_calificacionesAction($id_asignatura){
+                $request = Request::createFromGlobals();
+                $notas = $request->request->all();
+                $claves = array_keys($notas);
+                $em = $this->getDoctrine()->getManager();
+                $repository = $this->getDoctrine()->getRepository('SISigueBundle:ActividadAsignatura');
+                //var_dump($claves);
+                //die();
+                foreach ($claves as $clave){
+                    //Tengo toda la info para saber cual es en la propia clave,
+                    // y el dato en el valor del array.
+                    $identificadores = explode("_", $clave);
+                     //var_dump($identificadores);
+                    $id_asignatura2 = $identificadores[1];
+                    $id_alumno = $identificadores[2];
+                    $nombre_actividad = $identificadores[3];
+                    $nombre_actividad = str_replace("%/%"," ",$nombre_actividad);
+                     //var_dump($nombre_actividad);
+                    $actividad  = $repository->findOneBy( array('idAlumno' => $id_alumno, 'idAsignatura' => $id_asignatura2, 'nombre' => $nombre_actividad));
+                     //var_dump($actividad);
+                    $valor = $notas[$clave];
+                     //var_dump($claves);
+                    if($identificadores[0] === "nota"){
+                        $actividad->setNota($valor);
+                    }else{
+                        $actividad->setObservaciones($valor);
+                    }
+                    //var_dump($actividad);
+                    //die();
+                    $em->persist($actividad);
+                    
+                }
+                
+                $em->flush();
+                
+                return $this->redirect($this->generateUrl("si_sigue_calificar_profesor",array("id_asignatura"=>$id_asignatura)));
+            
+        }  
+         public function exportar_calificacionesAction($id_asignatura){
+                
+                $kernel = $this->get('kernel');
+                $dir_abs = self::getDireccionAbsoluta();
+                require_once $dir_abs . '/vendor/Excel/lib/src/Classes/PHPExcel.php';
+            
+                // Create new PHPExcel object
+               
+               $objPHPExcel = new \PHPExcel();
+
+               // Set document properties
+             
+               $objPHPExcel->getProperties()->setCreator("ProyectoSigue")
+                                                                        ->setLastModifiedBy("ProyectoSigue")
+                                                                        ->setTitle("Office 2007 XLSX Test Document")
+                                                                        ->setSubject("Office 2007 XLSX Test Document")
+                                                                        ->setDescription("Calificador automatico")
+                                                                        ->setKeywords("office 2007 openxml php");
+                                  
+               
+               //AQUI DEBO IMPRIMIR TODOS LOS DATOS QUE HAY QUE MOSTRAR EN LA TABLA//
+              $fila = "A";
+              $columna = 1;
+              $datos = self::listado_alumnos_actividad_asignatura($id_asignatura);
+             
+              $asignatura = $datos["asignatura"];
+             
+              $resultados = $datos["resultados"];
+              $lista_actividades = $datos["actividades"];
+               
+              //Primero las cabeceras//
+              $objPHPExcel->getActiveSheet()->setCellValue('A'.$columna, $asignatura->getNombre());
+              $objPHPExcel->getActiveSheet()->setCellValue('B'.$columna, $asignatura->getId());
+              $objPHPExcel->getActiveSheet()->setCellValue('A2', "Id Alumno");
+              $objPHPExcel->getActiveSheet()->setCellValue('B2', "Nombre");
+              $objPHPExcel->getActiveSheet()->setCellValue('C2', "Apellidos");
+              
+              $fila="D";
+              foreach($lista_actividades as $actividad){
+                   $objPHPExcel->getActiveSheet()->setCellValue($fila.$columna, $actividad["nombre"]);
+                   $objPHPExcel->getActiveSheet()->setCellValue($fila.($columna+1), $actividad["peso"]);
+                   $fila++;
+              }
+              $objPHPExcel->getActiveSheet()->setCellValue($fila.($columna+1), "TOKENS");
+              $fila++;
+              $objPHPExcel->getActiveSheet()->setCellValue($fila.($columna+1), "NOTA TOTAL(aprox)");
+              //XXXXXXXXXXXXXXXX
+              //var_dump($lista_actividades);
+              //die();
+                //AHORA LOS DATOS DE VERDAD.
+             $ac_nota = 0; 
+              $x  = 0;
+              $columna = 3;
+              
+             foreach($resultados as $filas){
+                    $fila= "A";
+                   $y = 0; 
+                   $objPHPExcel->getActiveSheet()->setCellValue($fila++.$columna, $filas["alumno"]->getIdAlumno());                    
+                    $objPHPExcel->getActiveSheet()->setCellValue($fila++.$columna, $filas["alumno"]->getNombre()); 
+                    $objPHPExcel->getActiveSheet()->setCellValue($fila++.$columna, $filas["alumno"]->getApellidos()); 
+                   $array_actividades = $filas["actividades"];
+                        foreach($array_actividades as $fila_actividad ){
+
+                      
+                        //Variables de control del input
+                        $valor= $fila_actividad->getNota();
+                        /*
+                        $id_asignatura2 = $asignatura->getId();
+                        $nombre_actividad = str_replace(" ", "%/%",$fila_actividad->getNombre());
+                        $observaciones = $fila_actividad->getObservaciones();
+                        $id_alumno = $fila_actividad->getIdAlumno()->getIdAlumno();
+                        */
+                 
+                       
+                        //LA PROPIA NOTA DE LA ACTIVIDAD
+                        $objPHPExcel->getActiveSheet()->setCellValue($fila++.$columna, $valor); 
+                    
+                     $ac_nota = $ac_nota +( $valor *  $fila_actividad->getPeso());
+                    $y++;
+                    }
+                        $codigos = $filas["codigos"];
+                         $codigos = $codigos[0];
+                   
+                      $objPHPExcel->getActiveSheet()->setCellValue($fila++.$columna, $codigos->getNum()) ;
+                     
+                      $objPHPExcel->getActiveSheet()->setCellValue($fila++.$columna, $ac_nota);
+                      $ac_nota = 0;
+                    
+                   
+               $columna++;
+            }
+
+
+               // Rename worksheet
+             
+               $objPHPExcel->getActiveSheet()->setTitle('Calificador automatico'.$id_asignatura);
+               $objPHPExcel->getActiveSheet()->getHeaderFooter()->setOddHeader('&L&G&C&HPlease treat this document as confidential!');
+               $objPHPExcel->getActiveSheet()->getHeaderFooter()->setOddFooter('&L&B' . $objPHPExcel->getProperties()->getTitle() . '&RPage &P of &N');
+               
+               // Set active sheet index to the first sheet, so Excel opens this as the first sheet
+               
+               $nombre_fichero =  "calificaciones".$id_asignatura.".xlsx";
+               $nombre_ruta = $dir_abs."/web/archivos/lista_calificaciones/".$nombre_fichero;
+               $objWriter = \PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
+               $path = self::getDireccionAbsoluta() ."/web/archivos/lista_calificaciones/". $nombre_fichero;
+               $objWriter->save($path);
+               
+               $content = file_get_contents($path);
+               $response = new Response();
+               $response->headers->set('Content-Type', 'file/xlsx');
+               $response->headers->set('Content-Disposition', 'attachment; filename='.$nombre_fichero);
+                $response->setContent($content);
+               
+              
+
+               return $response;
+               
+               //return $nombre_fichero;
+        }
+        
+         public function importar_calificacionesAction(){
+            $kernel = $this->get('kernel');
+            $dir_abs = self::getDireccionAbsoluta();
+            require_once $dir_abs . '/vendor/Excel/lib/src/Classes/PHPExcel.php';
+            
+             $request = Request::createFromGlobals();
+            /*PARA SUBIR UNA LISTA EXCEL DE ALUMNOS, HAY QUE:
+             * 1º PROCESAR EL FICHERO \/
+             * 2º GUARDAR CADA FILA EN LA TABLA ALUMNOS (con la entity Alumno) \/
+             * 3º GUARDAR EL COMBO PROFESOR-ASIGNATURA \/
+             * 
+             */
+            
+             $exito = "none";  
+             if( $kernel->getEnvironment() === "dev" ){
+                 /*Si quereis que funcione en vuestro desarrollo localhost cambiad esta ruta.*/                                               
+                 //DIEGO:
+                 $uploaddir = self::getDireccionAbsoluta()."/web/archivos/";
+                $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+               
+             }else{
+                $uploaddir = '/var/www/Symfony/web/archivos/';
+                $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+               
+                }               
+                if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+                   $exito ="true";
+                } else {
+                   $exito ="false";
+                }
+             /*Se ha cargado y vamos a procesarlo para pintarlo*/
+                    $inputFileName = $uploadfile;
+                                             
+                    /**  Identify the type of $inputFileName  **/
+                    $inputFileType = \PHPExcel_IOFactory::identify($inputFileName);
+                    /**  Create a new Reader of the type that has been identified  **/
+                    $objReader = \PHPExcel_IOFactory::createReader($inputFileType);
+                    
+                    $objPHPExcel = $objReader->load($inputFileName);
+
+                    $objWorksheet = $objPHPExcel->getActiveSheet();
+                    /*Recogemos todos los datos del formulario de creación de asignaturas.*/
+                    $nombre_asignatura = $request->request->get('nombre_asignatura', 'default');
+
+                    $curso = $request->request->get('curso', '1');
+                    $grupo = $request->request->get('grupo', 'A');
+
+                    $session = $this->get("session");
+                    $idprofesor =$session->get('idprofesor');
+                    /*Generamos la asignatura donde deben ser insertados los alumnos*/
+                    $em = $this->getDoctrine()->getManager();
+                    $asignatura = new Asignaturas();
+                    $asignatura->setCurso($curso);
+                    $asignatura->setGrupo($grupo);
+                    $asignatura->setNombre($nombre_asignatura);
+                    $em->persist($asignatura);
+                    $em->flush();
+                    
+                    foreach ($objWorksheet->getRowIterator() as $row) {                        
+
+                        $cellIterator = $row->getCellIterator();
+                        $cellIterator->setIterateOnlyExistingCells(true); // This loops all cells iterated.                       
+                        self::subir_alumno($cellIterator, $asignatura);                    
+                    }
+                   $profesor = $em->getRepository('SISigueBundle:Profesor')->find($idprofesor);
+                   $profesor_asignatura = new ProfesorAsignatura();
+                   $profesor_asignatura->setIdAsignatura($asignatura->getId());
+                   $profesor_asignatura->setIdProfesor($profesor);
+                   $em->persist($profesor_asignatura);
+                   $em->flush(); 
+                   return $this->indexAction("true");
+                
+        }
+        
+        
         public function generar_actividadAction(){
             $request = Request::createFromGlobals();
              
@@ -620,6 +808,51 @@ class ProfesorController extends Controller
                 $pdf->Image($codigo[0],$x,$y,80,80);
                 //pie de pagina
                 $pdf->Image($dir_abs.'/web/img/linea.jpg',$x,$y+75,100,2);
+            }
+            
+            private function listado_alumnos_actividad_asignatura($id_asignatura){
+                 $em = $this->getDoctrine()->getManager();
+                $repo = $em->getRepository('SISigueBundle:Asignaturas');
+                $asignatura = $repo->find($id_asignatura);  
+                //Tengo que sacar la lista de alumnos de la asignatura
+                //Luego, por cada alumno, saco la lista de actividades que tiene asociado
+                // y se lo añado a un array listo para mostrar.
+
+                $asig_alumnos = $em->getRepository('SISigueBundle:AsignaturaAlumno')->findByIdAsignatura($asignatura);
+                //Para poder mostrarlo correctamente necesito la info de las actividades, sin importar 
+                $actividades = $em->createQuery('
+                            SELECT distinct(p.nombre), p.descripcion, p.peso
+                            FROM SISigueBundle:ActividadAsignatura p                   
+
+                        ');
+                $actividades_info = $actividades->getResult();
+               // var_dump($actividades_info);
+               // die();
+                $resultado = array();
+                $parcial = array();
+                foreach($asig_alumnos as $alumnos){
+                    $alumno = $alumnos->getIdAlumno();
+                    //$repo_actividades = $em->getRepository('SISigueBundle:ActividadAsignatura');
+                    $query = $em->CreateQuery(
+                            'SELECT p
+                             FROM SISigueBundle:ActividadAsignatura p
+                                WHERE p.idAsignatura = :idAsignatura  
+                                AND p.idAlumno = :idAlumno
+                                '
+                            )->SetParameters(array('idAsignatura'=>$id_asignatura, 'idAlumno'=>$alumno));
+                    $actividades = $query->getResult();
+                    $query2 = $em->CreateQuery(
+                                'SELECT cod
+                                FROM SISigueBundle:AsignaturaAlumno cod
+                                where cod.idAsignatura = :idAsignatura
+                                AND cod.idAlumno = :idAlumno
+                                ')->SetParameters(array('idAsignatura'=>$id_asignatura, 'idAlumno'=>$alumno));
+                    $codigos = $query2->getResult();
+
+                    $parcial = array("alumno" => $alumno, "actividades" => $actividades, "codigos"=>$codigos);
+                    array_push($resultado, $parcial);
+                }
+                return array("asignatura"=>$asignatura,"resultados"=>$resultado, "actividades"=>$actividades_info);
             }
     }
   
