5eb8ebf47a39cc30f684dc23bfcb11ee39887df9
ramonnuche89@gmail.com <ramonnuche89@gmail.com@94efbc2a-9681-09ae-62ee-ccbec4989650>
2014-05-30 15:24:29 +0000

    Arreglado problema acceso a la base de datos.

--- trunk/web/web/Android_api/config.php
+++ trunk/web/web/Android_api/config.php
@@ -8,10 +8,10 @@
 
 define("DB_HOST", "127.0.0.1");
 
-define("DB_USER", "root");
+define("DB_USER", "admin");
 
-define("DB_PASSWORD", "admin00");
+define("DB_PASSWORD", "admin");
 
-define("DB_DATABASE", "SigueDB");
+define("DB_DATABASE", "siguedb");
 
 ?>
